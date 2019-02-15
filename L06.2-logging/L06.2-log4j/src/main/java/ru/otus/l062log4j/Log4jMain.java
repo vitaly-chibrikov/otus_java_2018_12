c2f462a82957e2d9e3b3bc33f6642e9083c2664d
egor shubin <egor.shubin@rovio.com>
2019-02-15 21:20:25 +0200

    webinar edits

--- L06.2-logging/L06.2-log4j/src/main/java/ru/otus/l062log4j/Log4jMain.java
+++ L06.2-logging/L06.2-log4j/src/main/java/ru/otus/l062log4j/Log4jMain.java
@@ -3,7 +3,7 @@ package ru.otus.l062log4j;
 import ru.otus.logging.l062logging.LoggingMain;
 
 public class Log4jMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         LoggingMain.main();
     }
 }
