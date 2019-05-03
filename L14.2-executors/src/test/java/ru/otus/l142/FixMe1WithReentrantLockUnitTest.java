f8129a99ddcb2a52c0edf40c0f5e13f7ca113fc3
Vladimir Krasilschik <bob.the.dyer@gmail.com>
2019-05-03 10:25:18 +0300

    L14.2-executors  - тест для лока

--- L14.2-executors/src/test/java/ru/otus/l142/FixMe1WithReentrantLockUnitTest.java
+++ L14.2-executors/src/test/java/ru/otus/l142/FixMe1WithReentrantLockUnitTest.java
@@ -3,9 +3,13 @@ package ru.otus.l142;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
+import static java.lang.String.format;
 import static java.lang.System.out;
 
 //TODO 1) FIXME with ReeentrantLock
@@ -13,41 +17,63 @@ import static java.lang.System.out;
 // 3) conditions
 public class FixMe1WithReentrantLockUnitTest {
     @Test
-    public void testMonitorWorksGreat() throws InterruptedException {
+    public void testReentrantLockWorksGreat() throws InterruptedException {
+        out.println("start");
 
         long start = System.currentTimeMillis();
         final List<Integer> list = new ArrayList<>();
         List<Throwable> throwables = new ArrayList<>();
+        CountDownLatch latch = new CountDownLatch(1);
 
         Thread t1 = new Thread(() -> {
+            int count = 0;
             try {
-                for (int i = 0; i < 10000; i++) {
+                latch.await();
+                for (int i = 0; i < 1000; i++) {
+                    count++;
                     list.add(i);
                 }
             } catch (Throwable throwable) {
                 throwables.add(throwable);
             }
+            out.println(format("add called %s times", count));
         });
         Thread t2 = new Thread(() -> {
+            int count = 0;
             try {
+                latch.await();
                 for (int i = 0; i < 1000; i++) {
-                    list.forEach(out::println);
+                    count++;
+                    sum(list);
                 }
             } catch (Throwable throwable) {
                 throwables.add(throwable);
             }
+            out.println(format("sum called %s times", count));
         });
 
         t1.start();
         t2.start();
 
+        latch.countDown();
+
         t1.join();
         t2.join();
 
-        out.println(String.format("run time = %s millis", (System.currentTimeMillis() - start)));
+        out.println(format("execution time = %s millis", (System.currentTimeMillis() - start)));
 
         if (!throwables.isEmpty()) {
             Assert.fail(throwables.toString());
         }
+    }
+
+    private void sum(List<Integer> list) {
+        Iterator<Integer> iterator = list.iterator();
+        BigDecimal sum = new BigDecimal(0);
+        while (iterator.hasNext()) {
+            Integer next = iterator.next();
+            sum = sum.add(BigDecimal.valueOf(next));
+        }
+        out.println(sum);
     }
 }
